bd4c868905f7be084dc2ff492552db2423ede32f
Eric Shahan <ctibalt@gmail.com>
2023-11-11 16:39:37 -0600

    Minor Bugfixes

--- src/main/java/org/launchcode/techjobs/persistent/controllers/EmployerController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/EmployerController.java
@@ -49,6 +49,7 @@ public class EmployerController {
         if (optEmployer.isPresent()) {
             Employer employer = (Employer) optEmployer.get();
             model.addAttribute("employer", employer);
+//            model.addAttribute("employer.jobs", employer.get);
             return "employers/view";
         } else {
             return "redirect:../";
